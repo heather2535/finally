b3d4997b86f72def69a1f71299c20a50835f6e44
heather2535 <heather.davies253@gmail.com>
2024-12-19 11:44:23 -0800

    First commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,6 +17,7 @@ import {
   docker,
   meta,
   starbucks,
+
   tesla,
   shopify,
   carrent,
@@ -50,11 +51,11 @@ const services = [
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "User Experience Research",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Product Designer",
     icon: creator,
   },
 ];
@@ -116,55 +117,54 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
+    title: "Lead User Experience Designer",
+    company_name: "Talentora",
     icon: tesla,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "September 2024 - Current",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Led all design decision-making for team of five developers and two designers, creating a groundbreaking app that leverages conversational AI assessments to streamline the first-round hiring process, enabling recruiters to find the perfect candidates faster.",
+      "Designed brand/marketing identity and user interface, creating 15 unique assets including logos, promotional media, and icons, resulting in an increased brand recognition.",
+      "Modeled and linked over 50+ screens with a final understanding presentation to facilitate clean design handoff.",
+      "Worked closely with developers to create a working pilot and current beta release.",
+    ],
+    
+  },
+  {
+    title: "UI/UX Designer",
+    company_name: "Boston University – AI Task Force Policy Dashboard",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "Novmeber 2023 - April 2024",
+    points: [
+      "Designed an intuitive, data-driven dashboard for analysing ethical AI practices, providing more than 100 leading resources, a semantic graph, and topic models.",
+      "Integrated tools such as ShadCN and MUI for front-end, CMS for organization of resource filtering , and node.js and react for data visualization.",
+      "Partnered with developers to design and integrate interactive tools for over 400 researchers and faculty to analyze ethical AI use.",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Shopify",
+    title: "UX Design Intern",
+    company_name: "Cervera",
     icon: shopify,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "June 2024 - August 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Crafted impactful real estate marketing campaigns across landing pages, organic assets, and print materials, leading to a $15,000 quarterly increase in agent sales.",
+      "Developed nationwide branding materials, building a unified outlook across diverse brand guidelines and effectively managing multiple projects simultaneously across cross-functional teams.",
+      "Accomplished an increase in user engagement, as measured by improved click-through rates and session durations, by utilizing a command of visual design, typography, color theory, and visual storytelling to create compelling and cohesive user interfaces.",
     ],
   },
   {
-    title: "Full stack Developer",
-    company_name: "Meta",
+    title: "Lead User Experience Designer",
+    company_name: "Boston Children's Hospital",
     icon: meta,
     iconBg: "#E6DEDD",
     date: "Jan 2023 - Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Completed a comprehensive competitor analysis, measuring market share across 10 key competitors and identifying a 15% gap in feature differentiation. This positioned our product for an increase in market penetration",
+      "Communicated interaction and design ideas to prospective shareholders through the development of critical mockups, detailed storyboards, and prototypes that address user stories.",
+      "Crafted intuitive user interfaces for 2 major projects based on close collaboration with the client, resulting in a 30% improvement in end-user satisfaction and usability.",
     ],
   },
 ];
@@ -232,13 +232,35 @@ const projects = [
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "css",
         color: "pink-text-gradient",
       },
     ],
     image: jobit,
     source_code_link: "https://github.com/",
   },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+
   {
     name: "Trip Guide",
     description:
